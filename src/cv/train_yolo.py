51f0d6f04db303c28e004ab9088858e329fd70ca
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2025-05-20 18:12:22 +0200

    new yolo training

--- src/cv/train_yolo.py
+++ src/cv/train_yolo.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 1️⃣  Path to your data.yaml (edit this)
-DATA_YAML = "/home/philipp/projects/hecky2/data/exported_labels/yolo_with_images/dataset/data.yaml"   # contains train/val paths + names
+DATA_YAML = "/home/philipp/projects/hecky2/data/dataset/data.yaml"   # contains train/val paths + names
 
 # 2️⃣  Load the YOLO model (nano variant)
 model = YOLO("model_checkpoints/yolo11n.pt")
@@ -9,10 +9,10 @@ model = YOLO("model_checkpoints/yolo11n.pt")
 # 3️⃣  Train on CPU
 results = model.train(
     data=DATA_YAML,   # your dice dataset
-    epochs=5,
+    epochs=100,
     imgsz=640,
     device="cpu",     # force CPU
-    batch=8,          # keep batch small for CPU
+    batch=16,          # keep batch small for CPU
     workers=0         # avoid dataloader overhead on single-CPU
 )
 
